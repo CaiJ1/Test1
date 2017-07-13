59542f18fc978feb069c3373c857a242eac254f7
jiacai <jiacai@jd.com>
2017-07-13 17:20:15 +0800

    swap

--- PracticesOne/main.mm
+++ PracticesOne/main.mm
@@ -15,21 +15,49 @@
 //#include "jiacai.h"
 using namespace std;
 
+void swapr(int &a, int &b);     //a, b are aliases for ints
+void swapp(int *p, int *q);     //p, q are addresses of ints
+void swapv(int a, int b);       //a, b are new variables
+
 void test() {
   
-    int rats = 101;
-    int &rodents = rats;    //rodents is a reference
-    cout << "rats = " << rats;
-    cout << ", rodents = " << rodents << endl;
+    int wallet1 = 300;
+    int wallet2 = 350;
+    cout << "wallet1 = $" << wallet1;
+    cout << " wallet2 = $" << wallet2 << endl;
     
-    rodents ++;
-    cout << "rats = " << rats;
-    cout << ", rodents = " << rodents << endl;
+    cout << "Using references to swap contents:\n";
+    swapr(wallet1, wallet2);
+    cout << "wallet1 = $" << wallet1;
+    cout << " wallet2 = $" << wallet2 << endl;
     
-    //some implementations require type casting the following addresses to type unsigned
-    cout << "rats address = " << &rats;
-    cout << ", rodents address = " << &rodents << endl;
+    cout << "Using pointers to swap contents again:\n";
+    swapp(&wallet1, &wallet2);
+    cout << "wallet1 = $" << wallet1;
+    cout << " wallet2 = $" << wallet2 << endl;
     
+    cout << "Trying to use passing by value:\n";
+    swapv(wallet1, wallet2);
+    cout << "wallet1 = $" << wallet1;
+    cout << " wallet2 = $" << wallet2 << endl;
+}
+
+void swapr(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+void swapp(int *p, int *q) {
+    int temp;
+    temp = *p;
+    *p = *q;
+    *q = temp;
+}
+void swapv(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main(int argc, const char * argv[]) {
